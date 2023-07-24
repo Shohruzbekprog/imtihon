f01f645d86cff83c19026efc8c1f05c6d44e625f
Shohruzbekprog <shokhruz.akhe@gmail.com>
2023-07-24 13:08:13 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$('.owl-carousel').owlCarousel({
+  center: false,
+  items:3,
+  loop:true,
+  margin:10,
+  nav: true,
+  dots: false,
+  responsive:{
+     0: {
+        items: 1,
+      },
+      800: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+  }
+  
+});
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add('backtop-show')
+  } else {
+    backtop.classList.remove('backtop-show')
+  }
+}
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+
+
